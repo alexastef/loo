db932843e261fdce47159cb752922bc78284f721
Erin Smith <erin.acumen@gmail.com>
2020-07-09 17:20:22 -0700

    passport/middleware

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,27 @@
 var path = require("path");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 
 module.exports = function(app) {
   app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname,"../public/html/maptest.html"));
+  });
+  app.get("/", function(req, res) {
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/login", function(req, res) {
+
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 };
