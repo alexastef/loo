aa1264873bb149b1f5cc4894b0d461d3d0ac3efb
Johnny Li <reptile18@gmail.com>
2020-07-11 12:59:51 -0700

    fixes to get us up and running

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
   // Getting references to our form and inputs
-<<<<<<< HEAD
-  var loginForm = $("form.loginForm");
-  var emailInput = $("input#userEmail");
-  var passwordInput = $("input#userPassword");
-=======
   var loginForm = $("form.login");
   var emailInput = $("input#inputEmail");
   var passwordInput = $("input#inputPassword");
->>>>>>> d61f18972a40d74fe3057bb3cadd6662d5958ff0
 
   // When the form is submitted, we validate there's an email and password entered
   loginForm.on("submit", function(event) {
@@ -30,28 +24,16 @@ $(document).ready(function() {
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
   function loginUser(email, password) {
-<<<<<<< HEAD
-    $.post("/api/login.handlebars", {
-=======
     $.post("/api/login", {
->>>>>>> d61f18972a40d74fe3057bb3cadd6662d5958ff0
       email: email,
       password: password
     })
       .then(function() {
-<<<<<<< HEAD
-        window.location.replace("/add");
-=======
-        window.location.replace("/addaloo");
->>>>>>> d61f18972a40d74fe3057bb3cadd6662d5958ff0
+        window.location.replace("/search");
         // If there's an error, log the error
       })
       .catch(function(err) {
         console.log(err);
       });
   }
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> d61f18972a40d74fe3057bb3cadd6662d5958ff0
