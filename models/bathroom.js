951130ba7c6d595db7f804fe654a23e59682bef8
Matt Moncayo <matthew.moncayo@gmail.com>
2020-07-11 14:03:55 -0700

    wip for models dir with associations

--- models/bathroom.js
+++ models/bathroom.js
@@ -1,34 +1,33 @@
 module.exports = function (sequelize, DataTypes) {
-    let Bathroom = sequelize.define("bathroom", {
-        place_id: DataTypes.INTEGER,
-        last_verified: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW
-        },
-        location_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        street_address: {
-            type: DataTypes.STRING
-        },
-        available: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        needs_key: DataTypes.BOOLEAN,
-        gender_neutral: DataTypes.BOOLEAN,
-        handicap_accessible: DataTypes.BOOLEAN,
-        has_water: DataTypes.BOOLEAN,
-        has_soap: DataTypes.BOOLEAN,
-        has_paper: DataTypes.BOOLEAN,
-        has_mirror: DataTypes.BOOLEAN,
-        thumbs_up: DataTypes.INTEGER,
-        thumbs_down: DataTypes.INTEGER
-    });
-    return Bathroom;
+  let Bathroom = sequelize.define("bathroom", {
+    place_id: DataTypes.INTEGER,
+    last_verified: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    location_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    street_address: {
+      type: DataTypes.STRING
+    },
+    available: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    needs_key: DataTypes.BOOLEAN,
+    gender_neutral: DataTypes.BOOLEAN,
+    handicap_accessible: DataTypes.BOOLEAN,
+    has_water: DataTypes.BOOLEAN,
+    has_soap: DataTypes.BOOLEAN,
+    has_paper: DataTypes.BOOLEAN,
+    has_mirror: DataTypes.BOOLEAN,
+    thumbs_up: DataTypes.INTEGER,
+    thumbs_down: DataTypes.INTEGER
+  });
+  return Bathroom;
 };
-  
