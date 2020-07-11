3088d8dcca73c71774b5c5f4bd9c6c7b5dd697e0
Johnny Li <reptile18@gmail.com>
2020-07-11 12:40:51 -0700

    detailed results including photos

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,18 +5,34 @@ const db = require("../models");
 const passport = require("../config/passport");
 
 module.exports = function(app) {
+  async function placeDetails (places) {
+    const promises = places.map(async place => {
+      const fields = "name,formatted_phone_number,formatted_address,geometry,photo,place_id"
+      const detailedParams = `place_id=${place.place_id}&fields=${fields}&key=${process.env.MAPS_API_KEY}`;
+      const detailedQuery = `https://maps.googleapis.com/maps/api/place/details/json?${detailedParams}`;
+
+      const response = await axios.get(detailedQuery);//.then((detailedPlace) => {
+      return response.data.result;
+    });
+    const detailedPlaces = await Promise.all(promises);
+    console.log(detailedPlaces);
+    //console.log("this number of places: " + detailedPlaces.length);
+    return detailedPlaces;
+  }
   app.get("/api/nearby/", (req,res) => {
     const lat = parseFloat(req.query.lat);
     const lon = parseFloat(req.query.lon);
 
     const params = `key=${process.env.MAPS_API_KEY}&location=${lat},${lon}&rankby=distance&type=store`
-
     axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?${params}`)
-    .then(r => {
-      console.log(r.data.results);
-      res.json(r.data.results);
+    .then(async r => {
+      //console.log(r.data.results);
+      //res.json(r.data.results);
+      const places = r.data.results;
+      const detailedPlaces = await placeDetails(places);
+      console.log(detailedPlaces);
+      res.json(detailedPlaces);
     });
-
   });
   app.get("/logout", function(req, res) {
     req.logout();
