e4c1393e896e80c0e2f26a86056b05c4e9c00754
Erin Smith <erin.acumen@gmail.com>
2020-07-10 21:41:26 -0700

    start help w handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,4 +5,4 @@ VALUES (null, "Frazier Farms Grocery", "1820 Oceanside Blvd, Oceanside, CA 92054
 (null, "Lazy Acres Market", "150 Encinitas Blvd, Encinitas, CA 92024", true, true, true, true, true, 1, 0),
  (null, "Shell Station", "2741 Gateway Rd, Carlsbad, CA 92009", true, true, true, true, true, 1, 0),
   (null, "Magdalena Ecke Family YMCA Park Bathroom", "200 Saxony Rd, Encinitas, CA 92024", true, true, false, false, false, 0, 1),
-  (null, "Oceanside City Beach", "Oceanside, CA 92024", true, false, false, false, false, 0, 1),
+  (null, "Oceanside City Beach", "Oceanside, CA 92054", true, true, true, false, false, 0, 1)
