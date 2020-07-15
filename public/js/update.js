16e256af8fd52314b05d03a37407e16fe3a62e99
Alexa Stefankiewicz <alexa.c.stef@gmail.com>
2020-07-14 20:08:37 -0700

    update script manipulates view and captures data, no post request yet

--- public/js/update.js
+++ public/js/update.js
@@ -1,19 +1,69 @@
 $(document).ready(() => {
-    $.ajax("/api/")
+
+  // when edit button is clicked
+    $("#editLooBtn").on("click", (event) => {
+
+        // toggles are enabled
+        $(".custom-control-input").removeAttr("disabled");
+
+        // range is enabled
+        $("#cleanRange").removeAttr("disabled");
+
+        // edit button becomes the update button
+        $("#editLooBtn").text("Update Loo");
+        $("#editLooBtn").attr("id", "updateLooBtn");
+
+        // prevent from resetting
+        event.preventDefault();
+
+    });
+
+    // when update button is clicked
     $("#updateLooBtn").on("click", (event) => {
-        // this will just enable all the toggles
+      event.preventDefault();
+
+      let name = $("h1");
+      let locationName = name[0].innerText;
+
+      let address = $(".streetAddress");
+      let locationAddress = address[0].innerText;
+
+      let phone = $(".placePhone");
+      let locationPhone = phone[0].innerText;
+
+      // pull new information from form
+      let updatedLoo = {
+        place_id: $("form.looForm").data("place-id"),
+        location_name: locationName,
+        street_address: locationAddress,
+        location_phone: locationPhone,
+        // basics
+        available: $("#looAvailable").is(":checked"),
+        needs_key: $("#needsKey").is(":checked"),
+        gender_neutral: $("#gender").is(":checked"),
+        handicap_accessible: $("#accessible").is(":checked"),
+        // supplies
+        has_water: $("#hasWater").is(":checked"),
+        has_soap: $("#hasSoap").is(":checked"),
+        has_paper: $("#hasPaper").is(":checked"),
+        has_mirror: $("#hasMirror").is(":checked"),
+        clean_rating: $("cleanRange").val()
+      }
+
+      // new ajax call to make a put request
+
     });
 });
 
-function loadPlacePhoto() {
-    const photoReference = $("#place_img").data("photo-reference");
-    const placeName = $("#place_img").data("place-name");
-    console.log("photoReference",photoReference);
-    $.ajax({
-      url: `/api/photo/${photoReference}`,
-      method: "get",
-    }).then(photoSrc => {
-      $("#place_img").attr("src",photoSrc).attr("alt",`${placeName} Photo`)
-    });
-  }
-  loadPlacePhoto();
+// function loadPlacePhoto() {
+//     const photoReference = $("#place_img").data("photo-reference");
+//     const placeName = $("#place_img").data("place-name");
+//     console.log("photoReference",photoReference);
+//     $.ajax({
+//       url: `/api/photo/${photoReference}`,
+//       method: "get",
+//     }).then(photoSrc => {
+//       $("#place_img").attr("src",photoSrc).attr("alt",`${placeName} Photo`)
+//     });
+//   }
+//   loadPlacePhoto();
