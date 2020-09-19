30bead4f6bb67bdec80020599c81d5773163f766
Johnny Li <reptile18@gmail.com>
2020-09-18 21:09:26 -0700

    click to relocate

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
+import axios from 'axios';
 import "./style.css";
 
 const mapStyles = {
@@ -9,6 +10,56 @@ const mapStyles = {
 };
 
 function Home(props) {
+  const [loos, setLoos] = useState([]);
+  const [center, setCenter] = useState({ lat: 32.715, lng: -117.1625 });
+  const [showRelocateModal, setShowRelocateModal] = useState(false);
+  // load loos
+  useEffect(() => {
+    console.log("process.env", process.env);
+  }, []);
+  // function displayDBCards(places) {
+  //   places.forEach((place, index) => {
+  //     createMarker(place, 200 * index);
+  //   });
+  // }
+  // function relocate(pos) {
+  //   if (source === 'home') {
+  //     showToast('Loading loos...');
+  //   }
+  //   else {
+  //     showToast('Loading places...');
+  //   }
+
+  //   console.log("relocate was given:",pos);
+
+  //   clearEverything();
+
+  //   currentLocationInfoWindow.setPosition(pos);
+  //   currentLocationInfoWindow.setContent('You are here.');
+  //   currentLocationInfoWindow.open(map);
+  //   map.setCenter(pos);
+
+  //   // send location to api route
+  //   $.ajax({
+  //     url: `/api/nearby/${source}?lat=${pos.lat}&lon=${pos.lng}`,
+  //     method: "get",
+  //   }).then(data => {
+  //     console.log(data);
+  //     hideToast();
+  //     displayPlaces(data, map);
+  //   });
+  // }
+
+  function mapClicked(mapProps, map, clickEvent) {
+    console.log("lat", clickEvent.latLng.lat());
+    console.log("lon", clickEvent.latLng.lng());
+    const confirmed = window.confirm("Search for establishments here?");
+    if (confirmed) {
+      setCenter({ lat: clickEvent.latLng.lat(), lng: clickEvent.latLng.lng() });
+    }
+    //relocate();
+  }
+
   return (
     <div className="container">
       {/*<!-- Row 1 (map and locations all one row with sub-rows) -->*/}
@@ -43,15 +94,11 @@ function Home(props) {
                 containerStyle={mapStyles.containerStyle}
                 google={props.google}
                 zoom={15}
-                initialCenter={
-                  {
-                    lat: 32.715,
-                    lng: -117.1625
-                  }
-                }
+                center={center}
+                onClick={mapClicked}
               ></Map>
             </div>
-            
+
             {/* <div id="map" className="d-inline-flex p-2"></div> */}
             <div className="row">
               <a href="/search" className="btn btn-warning btn-lg active" role="button" aria-pressed="true" id="addNew"><i
@@ -65,8 +112,27 @@ function Home(props) {
           </div>
         </div>
       </div>
+      <div class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Relocate</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>Search for loos at this location?</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary">Yes</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default GoogleApiWrapper({apiKey: process.env.MAPS_API_KEY})(Home);
+export default GoogleApiWrapper({ apiKey: process.env.REACT_APP_MAPS_API_KEY })(Home);
