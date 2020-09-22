cda51f302f3e5385dd25ce0fac14da940835f5c8
Matt Moncayo <matthew.moncayo@gmail.com>
2020-09-22 10:35:59 -0700

    redirected logout to pages dir

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
-import Logout from './components/Logout';
+import Logout from './pages/Logout';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import Search from './pages/Search';
